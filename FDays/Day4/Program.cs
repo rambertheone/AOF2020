16f709973070832ca8b022f11fcd081e9f5d1d37
rambertheone <123188440+rambertheone@users.noreply.github.com>
2023-10-06 17:26:35 -0400

    commit with the first four days completed

---
+++ FDays/Day4/Program.cs
@@ -0,0 +1,147 @@
+﻿using System.Linq;
+
+namespace adventofcode;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Day4();
+    }
+    static void Day4()
+    {
+
+        string[] content = File.ReadAllLines("../../../adventofcode.com_2020_day_4_input.txt");
+        //string[] content = File.ReadAllLines("../../../Untitled.txt");
+        int spaces = 0;
+        int complete = 0;
+
+        for (int i = 0; i < content.Length; i++)
+        {
+            if (content[i] == "")
+            {
+                spaces++;
+            }
+        }
+
+        int place = 0;
+        string words = "";
+        int valid = 0;
+        string[] content2 = new string[spaces + 1];
+
+        for (int i = 0; i < content.Length; i++)
+        {
+
+            if (content[i] != "" )
+            {
+                words = content[i] + " " + words;
+            }
+            else
+            {
+                content2[place] = words;
+                place++;
+                words = "";
+                
+            }
+            if (content.Length -1 == i)
+            {
+                content2[place] = words;
+            }
+
+
+        }
+
+        for (int i = 0; i < content2.Length; i++)
+        {
+            
+            if (content2[i].Contains("ecl") == true && content2[i].Contains("pid") == true && content2[i].Contains("eyr") == true && content2[i].Contains("hcl") == true && content2[i].Contains("byr") == true && content2[i].Contains("iyr") == true && content2[i].Contains("hgt"))
+            {
+                for(int k=0; k < content2.Length; k++)
+                {
+
+                    string[] value = content2[i].Split(' ');
+                    
+                    string[,] values2 = new string[value.Length - 1, 2];
+                    int iteration = 0;
+                    int iteration2 = 0;
+                    for(int j=0; j < value.Length - 1; j++)
+                    {
+                       
+                        string[] values = value[j].Split(':');
+                        values2[j, iteration2] = values[iteration2];
+                        iteration2++;
+                        iteration++;
+                        values2[j, iteration2] = values[iteration2];
+                        iteration2 = 0;
+                        
+                    }
+                    
+
+                    if( k == content2.Length - 1)
+                    {
+                        for (int n = 0; n < values2.GetLength(0); n++)
+                        {
+                            for (int m = 0; m < values2.GetLength(1) - 1; m++)
+                            {
+                                if (values2[n, m] == "byr" && int.Parse(values2[n, m + 1]) < 2003)
+                                {
+                                    valid++;
+                                }
+                                else if (values2[n, m] == "hgt")
+                                {
+
+                                    if (values2[n, m + 1].Contains("cm") == true)
+                                    {
+                                        if (values2[n, m + 1].Count() == 5)
+                                        {
+                                            valid++;
+                                        }
+                                    }
+                                    else if (values2[n, m + 1].Contains("in") == true)
+                                    {
+                                        if (values2[n, m + 1].Count() == 4)
+                                        {
+                                            valid++;
+                                        }
+                                    }
+
+                                }
+                                else if (values2[n, m] == "hcl" && values2[n, m + 1].Contains("#") == true)
+                                {
+                                    valid++;
+                                }
+                                else if (values2[n, m] == "ecl" && values2[n, m + 1].Contains("brn") == true)
+                                {
+                                    valid++;
+                                }
+                                else if (values2[n, m] == "pid" && values2[n, m + 1].Count() == 9)
+                                {
+                                    valid++;
+                                }
+
+                            }
+                            if (valid == 5)
+                            {
+                                complete++;
+                                valid = 0;
+                            }
+
+                        }
+                    }
+
+                }
+              
+            }
+
+                
+        }
+        Console.WriteLine(complete);
+        Console.ReadLine();
+
+
+    }
+
+
+
+}
+
---
+++ FDays/Day_1-3/Program.cs
@@ -0,0 +1,134 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace AdventOfCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Day3();
+        }
+
+        static void Day1()
+        {
+            string[] array = File.ReadAllLines("../../../adventofcode.com_2020_day_1_input.txt");
+            int[] intArr = new int[array.Length];
+            int first = 0;
+            int second = 0;
+            int third = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                intArr[i] = int.Parse(array[i]);
+            }
+
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                for (int j = 0; j < intArr.Length; j++)
+                {
+                    for (int k = 0; k < intArr.Length; k++)
+                    {
+
+                        if (intArr[i] + intArr[j] + intArr[k] == 2020 && j != i && j != k && k != i)
+                        {
+                            first = intArr[i];
+                            second = intArr[j];
+                            third = intArr[k];
+                        }
+
+                    }
+                }
+            }
+
+            Console.WriteLine(first + " " + second + " " + third + " = " + first * second * third);
+        }
+
+        static void Day2()
+        {
+            string[] array = File.ReadAllLines("../../../adventofcode.com_2020_day_2_input.txt");
+            int count = 0;
+            int valid = 0;
+            char[] delimiterChars = { '-', ' ', ':' };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string[] words = array[i].Split(delimiterChars);
+
+
+         
+                
+                    if (words[2][0] == words[4][int.Parse(words[0]) - 1] && words[2][0] != words[4][int.Parse(words[1]) - 1])
+                    {
+                        valid++;
+                    }
+                    else if (words[2][0] == words[4][int.Parse(words[1]) - 1] && words[2][0] != words[4][int.Parse(words[0]) - 1])
+                    {
+                        valid++;
+                    }
+
+                
+
+
+
+            }
+            Console.WriteLine(valid);
+
+        }
+
+        static void Day3()
+        {
+            string[] array = File.ReadAllLines("../../../adventofcode.com_2020_day_3_input.txt");
+            int repetitions = 200;
+            char[,] DArray = new char[array.Length, array[0].Length * repetitions];
+            
+            for (int i = 0;i < array.Length;i++)
+            {
+                
+                Console.WriteLine(array[i]);
+                for (int j = 0;j < array[i].Length;j++)
+                {
+
+                    DArray[i, j] = array[i][j];
+                    for (int k = 1;k < repetitions;k++)
+                    {
+                        DArray[i, (array[0].Length) * k + j] = array[i][j];
+                    }
+                    
+                }
+
+            }
+
+
+
+            int y = 0;
+            int x = 0;
+            int trees = 0;
+
+            for (int row = 0; row < DArray.GetLength(0);row++) 
+            {
+                if (y < DArray.GetLength(0) && x < DArray.GetLength(1))
+                {
+                    if (DArray[y, x] == '#')
+                    {
+                        trees++;
+                        
+                        
+                    }
+                }
+                
+                y+=1;
+                x += 7;
+                
+                
+                
+                
+                
+
+
+            }
+            Console.WriteLine(trees);
+        }
+    }
+}
