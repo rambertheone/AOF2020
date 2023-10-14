cd90ef9fa8794f42b0fc442e702623772fc5a3a4
rambertheone <123188440+rambertheone@users.noreply.github.com>
2023-10-14 03:43:17 -0400

    commit with advent of code day 7 part 2 completed

---
+++ adventofcode/adventofcode/Bag.cs
@@ -0,0 +1,43 @@
+﻿namespace test
+{
+    internal class Bag
+    {
+        private string _color;
+        private string[] _containedBagColors;
+        private int[] _containedBagCounts;
+
+        public Bag(string color_, string[] containedBagColors_, int[] containedBagCounts_)
+        {
+            _color = color_;
+            _containedBagColors = containedBagColors_;
+            _containedBagCounts = containedBagCounts_;
+        }
+
+        // Checks direct containment only
+        public bool Contains(string keyColor)
+        {
+            foreach (string color in ContainedBagColors)
+            {
+                if (color == keyColor)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public string Color
+        {
+            get { return _color; }
+        }
+
+        public string[] ContainedBagColors
+        {
+            get { return _containedBagColors; }
+        }
+
+        public int[] ContainedBagCounts
+        {
+            get { return _containedBagCounts; }
+        }
+    }
+}
