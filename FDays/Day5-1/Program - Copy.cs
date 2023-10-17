70d525dd3fbf82542d9d19fd2e720dbdd3d71530
Tsafack Fadia <2265786@johnabbottcollege.net>
2023-10-17 18:50:25 -0400

    commit with day five part 1 version 2 done

---
+++ FDays/Day5-1/Program - Copy.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace adventofcode;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string file = "../../../adventofcode.com_2020_day_5_input.txt";
+        //string[] fileInfo = File.ReadAllLines(file);
+        string[] fileInfo = { "BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL" };
+
+        int[] data = new int[fileInfo.Length];
+        int result = 0;
+
+        int dataInside = 0;
+
+
+        for (int i = 0; i < fileInfo.Length; i++)
+        {
+           
+            string row1 = "";
+            string col1 = "";
+
+            row1 += fileInfo[i].Replace('F', '0').Replace('B', '1').Replace('R', '1').Replace('L', '0');
+
+            Console.WriteLine(Convert.ToInt32(row1, 2));
+
+
+        }
+
+
+        Console.WriteLine(dataInside);
+        Console.ReadLine();
+
+    }
+}
+
+
